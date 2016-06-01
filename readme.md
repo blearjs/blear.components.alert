28aa7bbcac16100c3d5ae9e45b2b001c231de0af
#云淡然 <cloudcome@163.com>
2016-06-01 14:40:44 +0800

    blear.components.alert

---
+++ readme.md
@@ -0,0 +1,15 @@
+# blear.components.alert
+
+[![npm module][npm-img]][npm-url]
+[![build status][travis-img]][travis-url]
+[![coverage][coveralls-img]][coveralls-url]
+
+[travis-img]: https://img.shields.io/travis/blearjs/blear.components.alert/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/blearjs/blear.components.alert
+
+[npm-img]: https://img.shields.io/npm/v/blear.components.alert.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/blear.components.alert
+
+[coveralls-img]: https://img.shields.io/coveralls/blearjs/blear.components.alert/master.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/github/blearjs/blear.components.alert?branch=master
+
---
+++ src/index.js
@@ -0,0 +1,41 @@
+/**
+ * alert 组件
+ * @author ydr.me
+ * @create 2016-05-30 21:13
+ */
+
+
+'use strict';
+
+
+var Alert = require('blear.ui.alert');
+var typeis = require('blear.utils.typeis');
+
+
+/**
+ * 弹出警告框
+ * @param message {String|Object} 字符串，或者是 Error 实例
+ * @returns {*|Window}
+ */
+module.exports = function (message) {
+    var options = {};
+
+    if (typeis.Object(message)) {
+        options = message;
+    } else {
+        message = message || '';
+        message = message && message.message ? message.message : message;
+        options.message = message;
+    }
+
+    var alert = new Alert(options);
+
+    alert.on('sure', function () {
+        alert.destroy(function () {
+            alert = null;
+        });
+    });
+
+    return alert.open();
+};
+
