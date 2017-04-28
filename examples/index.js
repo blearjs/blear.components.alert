bd8b40b35f5ef977ae79178fe30d1f5236bd901d
#云淡然 <cloudcome@163.com>
2017-04-28 11:04:26 +0800

    ~ 示例

---
+++ examples/index.js
@@ -0,0 +1,15 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2017-04-28 10:49
+ * @update 2017-04-28 10:49
+ */
+
+
+'use strict';
+
+var alert = require('../src/index');
+
+
+alert('字符串消息');
+alert(new Error('错误消息'));
